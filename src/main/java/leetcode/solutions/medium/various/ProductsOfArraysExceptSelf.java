de6663f5b77f5f2078a3af61c7508e002e67e37a
ValentinoFilipetto <valefilipetto@gmail.com>
2026-05-23 09:38:45 +0200

    Improve Products of Arrays Except Self solution

--- src/main/java/leetcode/solutions/medium/various/ProductsOfArraysExceptSelf.java
+++ src/main/java/leetcode/solutions/medium/various/ProductsOfArraysExceptSelf.java
@@ -7,28 +7,20 @@ package leetcode.solutions.medium.various;
  */
 
 public class ProductsOfArraysExceptSelf {
-        public int[] productExceptSelf(int[] nums) {
-                int n = nums.length;
-                int[] res = new int[n];
-                int[] prefix = new int[n];
-                int[] suffix = new int[n];
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int prefix = 1;
 
-                prefix[0] = 1;
-                suffix[n - 1] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
 
-                // calculate prefix
-                for (int i = 1; i < prefix.length; i++) {
-                    prefix[i] = nums[i - 1] * prefix[i - 1];
-                }
-
-                // calculate suffix
-                for (int i = n - 2; i >= 0; i--) {
-                    suffix[i] = nums[i + 1] * suffix[i + 1];
-                }
-
-                for (int i = 0; i < res.length; i++) {
-                    res[i] = prefix[i] * suffix[i];
-                }
-                return res;
+        int postfix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
     }
 }
