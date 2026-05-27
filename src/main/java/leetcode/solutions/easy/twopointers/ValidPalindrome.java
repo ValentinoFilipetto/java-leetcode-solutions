b2839e7c6931f570e0f360cc3dc9d306c0c17125
Valentino Filipetto <ae09vf@insim.biz>
2026-05-27 22:27:03 +0200

    Improve valid palindrome solution

--- src/main/java/leetcode/solutions/easy/twopointers/ValidPalindrome.java
+++ src/main/java/leetcode/solutions/easy/twopointers/ValidPalindrome.java
@@ -11,27 +11,27 @@ public class ValidPalindrome {
         int l = 0, r = s.length() - 1;
 
         while (l < r) {
-            while (l < r && !alphaNum(s.charAt(l))) {
-                l++;
-            }
-            while (r > l && !alphaNum(s.charAt(r))) {
-                r--;
-            }
+            while (l < r && !isAlphaNum(s.charAt(l))) l++;
+            while (r > l && !isAlphaNum(s.charAt(r))) r--;
 
             // The problem statement expects us to compare only lowercase characters.
             if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
                 return false;
             }
-
             // Do not forget to move pointers here too.
             l++; r--;
         }
         return true;
     }
 
-    public boolean alphaNum(char c) {
+    private boolean isAlphaNum(char c) {
         return (c >= 'A' && c <= 'Z' ||
                 c >= 'a' && c <= 'z' ||
                 c >= '0' && c <= '9');
     }
+
+    // Above method could also be
+    // private boolean isAlphaNum(char c) {
+    //  return isLetter(c) || isDigit(c);
+    // }
 }
