df9722662849cf3c17b8dfc3d6c8a462277bc854
Valentino Filipetto <ae09vf@insim.biz>
2026-08-01 18:17:14 +0200

    Improve Kth largest element in a stream

---
+++ src/main/java/leetcode/solutions/easy/heaps/KthLargestElementInAStream.java
@@ -0,0 +1,33 @@
+package leetcode.solutions.easy.heaps;
+
+import java.util.PriorityQueue;
+
+/**
+ * Pattern: Heaps (min-heap)
+ * Time complexity: constructor takes O(n * log k), where n is the number of values in nums
+ * add() takes O(log k) time complexity.
+ * Space complexity: O(k)
+ */
+
+public class KthLargestElementInAStream {
+    private final PriorityQueue<Integer> minHeap;
+    private final int k;
+
+    public KthLargestElementInAStream(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) minHeap.poll();
+        }
+    }
+
+    public int add(int val) {
+        minHeap.offer(val);
+
+        if (minHeap.size() > k) minHeap.poll();
+
+        return minHeap.peek();
+    }
+}
