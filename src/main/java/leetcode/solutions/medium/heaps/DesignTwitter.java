8b2bcda3ca8443e104269ea4f8e281f088f22ee6
Valentino Filipetto <ae09vf@insim.biz>
2026-08-09 14:46:03 +0200

    improve design twitter

--- src/main/java/leetcode/solutions/medium/heaps/DesignTwitter.java
+++ src/main/java/leetcode/solutions/medium/heaps/DesignTwitter.java
@@ -1,7 +1,6 @@
 package leetcode.solutions.medium.heaps;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -11,11 +10,13 @@ import java.util.Set;
 
 /**
  * Pattern: min-heap
- * Time complexity: postTweet(), follow() and unfollow() take O(1) time, getNewsFeed() takes O(f * t), where f
- * is the number of followers and t is the number of tweets.
- * Space complexity: 
+ * Time complexity: postTweet(), follow() and unfollow() take O(1) time, getNewsFeed() takes O(f * log f). 
+ * where f is the number of followers, for the frontier build-up.
+ * 
+ * Space complexity: O(N * m + N * M + n) where n is the total number of followers associated with userId,
+ * m is the maximum number of tweets per user, N is the total number of user ids and M is the maximum number of 
+ * followers per user.
  */
-
 class Twitter {
     private Map<Integer, List<int[]>> tweetsMap; // tweet: [tweetId, creationTime]
     private Map<Integer, Set<Integer>> followersMap;
@@ -29,17 +30,15 @@ class Twitter {
     
     public void postTweet(int userId, int tweetId) {
         int[] tweet = new int[] { tweetId, time };
-
         tweetsMap
             .computeIfAbsent(userId, k -> new ArrayList<>())
             .add(tweet);
-
         time++;
     }
     
     public List<Integer> getNewsFeed(int userId) {
         PriorityQueue<int[]> minHeap = new PriorityQueue<>(
-            (a, b) -> a[1] - b[1]
+            (a, b) -> b[1] - a[1]
         );
 
         // Make a copy, otherwise followers.add(userId) mutates the real map list.
@@ -50,29 +49,32 @@ class Twitter {
         // User should see their own tweets too.
         followers.add(userId);
 
+        // Build frontier (most recent tweets in each list of tweets) and push them to the min-heap.
         for (int follower : followers) {
             List<int[]> tweets = tweetsMap.get(follower);
 
             if (tweets == null) continue;
 
-            for (int[] tweet : tweets) {
-                minHeap.offer(tweet);
+            int index = tweets.size() - 1;
+            int[] tweet = tweets.get(index);
+            minHeap.offer(new int[] { tweet[0], tweet[1], follower, index - 1 });
+        }
 
-                // Remove oldest tweet when more than 10.
-                if (minHeap.size() > 10) minHeap.poll();
+        List<Integer> topTenTweets = new ArrayList<>();
+        while (!minHeap.isEmpty() && topTenTweets.size() < 10) {
+            // Take the most recent tweet and add to solution.
+            int[] topTweet = minHeap.poll();
+            topTenTweets.add(topTweet[0]);
+            
+            // Move index to the next most recent tweet in the same list and add it to minHeap.
+            // It could still be more recent than other tweets in the frontier and we do not want to miss it.
+            int topTweetIndex = topTweet[3];
+            if (topTweetIndex >= 0) {
+                int[] newTweet = tweetsMap.get(topTweet[2]).get(topTweet[3]);
+                minHeap.offer(new int[] { newTweet[0], newTweet[1], topTweet[2], topTweetIndex - 1 });
             }
         }
         
-        List<Integer> topTenTweets = new ArrayList<>();
-
-        while (!minHeap.isEmpty()) {
-            topTenTweets.add(minHeap.poll()[0]);
-        }
-
-        // Because min heap gives oldest -> newest,
-        // but news feed needs newest -> oldest.
-        Collections.reverse(topTenTweets);
-
         return topTenTweets;
     }
     
@@ -82,9 +84,7 @@ class Twitter {
     
     public void unfollow(int followerId, int followeeId) {
         Set<Integer> followees = followersMap.get(followerId);
-
         if (followees == null) return;
-
         followees.remove(followeeId);
     }
 }
