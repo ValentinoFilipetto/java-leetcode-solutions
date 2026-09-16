3e1c1cad6470f272bb7fb0905a3e200e41198a9f
Valentino Filipetto <ae09vf@insim.biz>
2026-09-16 09:16:11 +0200

    small improvement to subsets

--- src/main/java/leetcode/solutions/medium/backtracking/Subsets.java
+++ src/main/java/leetcode/solutions/medium/backtracking/Subsets.java
@@ -12,23 +12,22 @@ import java.util.List;
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> subset = new ArrayList<>();
-        dfs(nums, 0, subset, res);
+        dfs(nums, 0, new ArrayList<>(), res);
         return res;
     }
 
     private void dfs(int[] nums, int i, List<Integer> subset, List<List<Integer>> res) {
         if (i == nums.length) {
             // new ArrayList<>(subset) creates a "shallow copy" of subset, which means that elements are not copied, but the list itself is.
-            // The reason why we need a copy is that ArrayList is a reference type, and as such its reference
-            // would still be accessible in other recursive calls (e.g. when we pop from it), but we do not want that.
+            // The reason why we need a copy is that ArrayList is a reference type, and as such its reference would still 
+            // be accessible in other recursive calls (e.g. when we pop from it), but we do not want that.
             res.add(new ArrayList<>(subset));
             return;
         }
 
-        dfs(nums, i + 1, subset, res);
         subset.add(nums[i]);
         dfs(nums, i + 1, subset, res);
         subset.remove(subset.size() - 1);
+        dfs(nums, i + 1, subset, res);
     }
 }
