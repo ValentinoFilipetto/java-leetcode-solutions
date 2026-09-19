a8c09054833323ff92835329c3f34aeab467f68b
ValentinoFilipetto <valefilipetto@gmail.com>
2026-09-19 21:22:15 +0200

    minor changes to word search

--- src/main/java/leetcode/solutions/medium/backtracking/WordSearch.java
+++ src/main/java/leetcode/solutions/medium/backtracking/WordSearch.java
@@ -7,16 +7,18 @@ package leetcode.solutions.medium.backtracking;
  */
 
 public class WordSearch {
-    private int ROWS, COLS;
+    char[][] board;
+    int rows;
+    int cols;
 
     public boolean exist(char[][] board, String word) {
-        ROWS = board.length;
-        COLS = board[0].length;
+        this.board = board;
+        this.rows = board.length;
+        this.cols = board[0].length;
 
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                // We first check if the first character matches. This saves some time on large boards.
-                if (board[r][c] == word.charAt(0) && backtrack(board, r, c, word, 0)) {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (board[r][c] == word.charAt(0) && backtrack(r, c, word, 0)) {
                     return true;
                 }
             }
@@ -24,26 +26,19 @@ public class WordSearch {
         return false;
     }
 
-    private boolean backtrack(char[][] board, int r, int c, String word, int i) {
+    private boolean backtrack(int r, int c, String word, int i) {
         if (i == word.length()) return true;
-
-        if (r < 0 || c < 0 || r == ROWS || c == COLS ||
-                board[r][c] == '#' || board[r][c] != word.charAt(i)) {
+        if (r < 0 || c < 0 || r >= rows || c >= cols || 
+            board[r][c] == '#' || board[r][c] != word.charAt(i)) {
             return false;
         }
 
-        char temp = board[r][c];
-        // Remember: always modify grid in place when possible, instead of
-        // using a set to keep track of visited cells, as it is more memory-efficient.
         board[r][c] = '#';
-
-        boolean res =
-                backtrack(board, r + 1, c, word, i + 1) ||
-                        backtrack(board, r - 1, c, word, i + 1) ||
-                        backtrack(board, r, c + 1, word, i + 1) ||
-                        backtrack(board, r, c - 1, word, i + 1);
-
-        board[r][c] = temp; // restore original char.
+        boolean res = backtrack(r + 1, c, word, i + 1) ||
+                      backtrack(r - 1, c, word, i + 1) ||
+                      backtrack(r, c + 1, word, i + 1) ||
+                      backtrack(r, c - 1, word, i + 1);
+        board[r][c] = word.charAt(i);
         return res;
     }
 }
