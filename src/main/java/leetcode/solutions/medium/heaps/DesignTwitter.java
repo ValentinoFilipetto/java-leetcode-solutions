d21c1c15bc769bb89080b4e155801c3e473dc520
ValentinoFilipetto <valefilipetto@gmail.com>
2026-09-22 12:20:49 +0200

    Refine DesignTwitter

--- src/main/java/leetcode/solutions/medium/heaps/DesignTwitter.java
+++ src/main/java/leetcode/solutions/medium/heaps/DesignTwitter.java
@@ -9,13 +9,12 @@ import java.util.PriorityQueue;
 import java.util.Set;
 
 /**
- * Pattern: min-heap
+ * Pattern: max-heap
  * Time complexity: postTweet(), follow() and unfollow() take O(1) time, getNewsFeed() takes O(f * log f). 
  * where f is the number of followers, for the frontier build-up.
  * 
- * Space complexity: O(N * m + N * M + n) where n is the total number of followers associated with userId,
- * m is the maximum number of tweets per user, N is the total number of user ids and M is the maximum number of 
- * followers per user.
+ * Space complexity: O(Nt + NF + f) where N is the total number of user ids, t is the maximum number of tweets per user, 
+ * F is the maximum number of followers per user and f is the total number of followers associated with userId
  */
 class Twitter {
     private Map<Integer, List<int[]>> tweetsMap; // tweet: [tweetId, creationTime]
@@ -37,11 +36,11 @@ class Twitter {
     }
     
     public List<Integer> getNewsFeed(int userId) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
             (a, b) -> b[1] - a[1]
         );
 
-        // Make a copy, otherwise followers.add(userId) mutates the real map list.
+        // Make a copy, otherwise followers.add(userId) on line 50 will mutate the real map list.
         Set<Integer> followers = new HashSet<>(
             followersMap.getOrDefault(userId, new HashSet<>())
         );
@@ -49,7 +48,7 @@ class Twitter {
         // User should see their own tweets too.
         followers.add(userId);
 
-        // Build frontier (most recent tweets in each list of tweets) and push them to the min-heap.
+        // Build a `frontier` (most recent tweets in each list of tweets) and push them to the min-heap.
         for (int follower : followers) {
             List<int[]> tweets = tweetsMap.get(follower);
 
@@ -57,24 +56,24 @@ class Twitter {
 
             int index = tweets.size() - 1;
             int[] tweet = tweets.get(index);
-            minHeap.offer(new int[] { tweet[0], tweet[1], follower, index - 1 });
+            // Structure is { tweetId, time, follower, index of last (and latest) tweet }
+            maxHeap.offer(new int[] { tweet[0], tweet[1], follower, index - 1 });
         }
 
         List<Integer> topTenTweets = new ArrayList<>();
-        while (!minHeap.isEmpty() && topTenTweets.size() < 10) {
-            // Take the most recent tweet and add to solution.
-            int[] topTweet = minHeap.poll();
+        while (!maxHeap.isEmpty() && topTenTweets.size() < 10) {
+            // Take the most recent tweet and add it to solution.
+            int[] topTweet = maxHeap.poll();
             topTenTweets.add(topTweet[0]);
             
             // Move index to the next most recent tweet in the same list and add it to minHeap.
             // It could still be more recent than other tweets in the frontier and we do not want to miss it.
             int topTweetIndex = topTweet[3];
             if (topTweetIndex >= 0) {
-                int[] newTweet = tweetsMap.get(topTweet[2]).get(topTweet[3]);
-                minHeap.offer(new int[] { newTweet[0], newTweet[1], topTweet[2], topTweetIndex - 1 });
+                int[] newTweet = tweetsMap.get(topTweet[2]).get(topTweetIndex);
+                maxHeap.offer(new int[] { newTweet[0], newTweet[1], topTweet[2], topTweetIndex - 1 });
             }
         }
-        
         return topTenTweets;
     }
     
@@ -84,6 +83,7 @@ class Twitter {
     
     public void unfollow(int followerId, int followeeId) {
         Set<Integer> followees = followersMap.get(followerId);
+        // If followerId is not in followersMap, then followees will be null;
         if (followees == null) return;
         followees.remove(followeeId);
     }
